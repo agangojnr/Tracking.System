9e3aef03b371a0583836c2226516f22a238a1941
agango <walteragango@gmail.com>
2025-08-26 22:28:29 +0300

    Filtering Subresellers by resellerID, UserID and ALL

--- src/main/java/org/traccar/api/resource/SubResellerResource.java
+++ src/main/java/org/traccar/api/resource/SubResellerResource.java
@@ -1,12 +1,21 @@
 
 package org.traccar.api.resource;
 
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import org.traccar.api.ExtendedObjectResource;
+import org.traccar.model.Client;
+import org.traccar.model.Reseller;
 import org.traccar.model.Subreseller;
+import org.traccar.model.User;
+import org.traccar.storage.StorageException;
+import org.traccar.storage.query.Columns;
+import org.traccar.storage.query.Condition;
+import org.traccar.storage.query.Order;
+import org.traccar.storage.query.Request;
+
+import java.util.Collection;
+import java.util.LinkedList;
 
 @Path("subresellers")
 @Produces(MediaType.APPLICATION_JSON)
@@ -15,6 +24,30 @@ public class SubResellerResource extends ExtendedObjectResource<Subreseller> {
 
     public SubResellerResource() {
         super(Subreseller.class, "name");
+    }
+
+
+    @GET
+    @Path("query")
+    public Collection<Subreseller> get(@QueryParam("all") Boolean all,
+                                       @QueryParam("resellerId") Long resellerId,
+                                       @QueryParam("userId") Long userId) throws StorageException{
+
+        var conditions = new LinkedList<Condition>();
+
+        if (Boolean.TRUE.equals(all)) {
+            if (permissionsService.notAdmin(getUserId())) {
+                conditions.add(new Condition.Permission(User.class, getUserId(), baseClass));
+            }
+        } else if (resellerId != null && resellerId > 0) {
+            conditions.add(new Condition.Permission(Reseller.class, resellerId, Subreseller.class).excludeGroups());
+        }else if(userId != null && userId > 0){
+            conditions.add(new Condition.Permission(User.class, userId, Subreseller.class).excludeGroups());
+        }
+
+        return storage.getObjects(baseClass, new Request(
+                new Columns.All(), Condition.merge(conditions), new Order("name")
+        ));
     }
 
 }
